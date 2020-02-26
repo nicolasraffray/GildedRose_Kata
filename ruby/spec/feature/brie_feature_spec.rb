a1d0ec3da06b18a09a9c61bb1503860d77c3c1fc
nicolasraffray <nraffray@gmail.com>
2020-02-26 12:44:55 +0000

    brie feature test refactored and change after sellby tested

--- ruby/spec/feature/brie_feature_spec.rb
+++ ruby/spec/feature/brie_feature_spec.rb
@@ -2,22 +2,38 @@ require_relative './../../gilded_rose'
 
 describe 'Brie testing' do 
 
+  
   item = [Item.new("Aged Brie", 10, 0)]
   subject { GildedRose.new(item)}
   
-  it 'increases in value as the days pass to sell by' do 
-    subject.update_quality()
-    expect(subject.items[0].sell_in).to eq(9)
-    expect(subject.items[0].quality).to eq(1)
+  describe 'changes' do 
+    it 'increases in value as the days pass to sell by' do 
+      subject.update_quality()
+      expect(subject.items[0].sell_in).to eq(9)
+      expect(subject.items[0].quality).to eq(1)
+    end 
+
+    it 'increases by 2 after sell_in date' do 
+      i = 0 
+      while i < 10 do 
+        subject.update_quality()
+        i += 1
+      end 
+      p subject.update_quality()
+      p subject.items[0].quality
+      expect{subject.update_quality()}.to change{subject.items[0].quality}.by(2)
+    end
   end 
 
-  it 'the quality never goes above 50' do
-    i = 0  
-    while i < 29 do 
+  describe 'capping at 50' do 
+    it 'the quality never goes above 50' do
+      i = 0  
+      while i < 29 do 
+        subject.update_quality()
+        i += 1
+      end
       subject.update_quality()
-      i += 1
-    end
-    subject.update_quality()
-    expect(subject.items[0].quality).to eq(50)
-  end 
+      expect(subject.items[0].quality).to eq(50)
+    end 
+  end  
 end 
