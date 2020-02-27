7a3d6e1998fa67924db896e6b6044594798812a0
nicolasraffray <nraffray@gmail.com>
2020-02-27 12:30:53 +0000

    refactor of normal item unit spec

--- ruby/spec/Unit/normal_item_spec.rb
+++ ruby/spec/Unit/normal_item_spec.rb
@@ -1,19 +1,15 @@
 require_relative './../../gilded_rose'
 
 describe Normal do 
+  let(:item){double(name: "Fruit", sell_in: 6, quality: 3)}
+  subject{ Normal.new(item) }
   
   describe "Takes in an item object" do 
     it "inherits from the item class" do 
-      item = double(name: "Fruit", sell_in: 6, quality: 3)
-      normal_good =  Normal.new(item)
-      expect(normal_good.item).to eq item
+      expect(subject.item).to eq item
     end 
   end 
 
-  describe "#update_sell_in" do 
-    it "reduces sell_in date by 1" do 
-      
-    end 
-  end 
+ 
 
 end 
